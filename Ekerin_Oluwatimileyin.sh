bfab80dc8dd052126f2d525ce794a8c6ef5777be
Ekerin Oluwatimileyin <mosesekerin@gmail.com>
2024-12-22 22:12:45 +0000

    Edited my .sh file

---
+++ Ekerin_Oluwatimileyin.sh
@@ -0,0 +1,3 @@
+My name is Ekerin Oluwatimileyin Moses, and I am 23-year-old Nigerian student studying Chemical Engineering at Obafemi Awolowo University. However, I am also a student of AltSchool Africa offering a one year diploma certification in Cloud Engineering.
+
+My goals for joining AltSchool Africa are multifaceted. I aim to gain specialized knowledge by deepening my understanding of cloud engineering and related technologies through their structured and updated curriculum.
